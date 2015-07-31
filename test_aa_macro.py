7e31c3dd15df15e7771cd1a467eb88f72194c7d6
Ben fyngyrz <fyngyrz@gmail.com>
2015-07-31 14:29:59 -0600

    * Fixed indentation * changed 'input' (python reservced word) to 'textBlock' * Turned textBlock and 'expected' into multi-line texts in prep for increase

--- test_aa_macro.py
+++ test_aa_macro.py
@@ -5,14 +5,19 @@ from aa_macro import macro
 
 class TestAAMacro(unittest.TestCase):
 
-    def test_simple_style(self):
-        '''
-        Test a simple substitution style.
-        '''
-        input = '[style hello Why hello, [b], how are you?]{hello Ben}'
-        expected = 'Why hello, Ben, how are you?'
-        output = macro().do(input)
-        self.assertEqual(expected, output)
+	def test_simple_style(self):
+		"""
+Test a simple substitution style.
+"""
+		testBlock = """
+[style hello Why hello, [b], how are you?]{hello Ben}
+"""
+
+		expected = """
+Why hello, Ben, how are you?
+"""
+		output = macro().do(testBlock)
+		self.assertEqual(expected, output)
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
